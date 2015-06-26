fcc2fa5f4675bfefc5757cbd29311cf7168eda34
Duncan Neal <duncan.neal@gmail.com>
2015-06-25 23:14:29 -0400

    uGh. triangle project, I didnot miss you

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,18 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if (a < 1) or (b < 1) or (c < 1)
+  raise TriangleError if side_lengths_not_triangular?(a, b, c)
+  
+  return :equilateral if (a == b) and (a == c)
+  return :isosceles if (a == b) and (a != c)
+  return :isosceles if (a != b) and (b == c)
+  return :isosceles if (a == c) and (b != c)
+  return :scalene if (a != b) and (b != c) and (c != a)  
+end
+
+def side_lengths_not_triangular?(a, b, c)
+   ((a + b) <= c) || ((b + c) <= a) || ((a + c) <= b)  
 end
 
 # Error class used in part 2.  No need to change this code.
